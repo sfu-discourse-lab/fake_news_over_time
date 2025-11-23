a779d6484b21c1e5450d0653ff4bf81c3045fe20
Adam Podoxin <adam.podoxin@gmail.com>
2025-11-22 23:22:55 -0800

    Make stats test automatically decide if use parametric or non-parametric test

--- helpers.py
+++ helpers.py
@@ -3,6 +3,7 @@ from typing import Any
 import pandas as pd
 from spacy.tokens.token import Token
 from wordcloud import WordCloud
+from scipy.stats import normaltest, levene, f_oneway, kruskal
 
 
 def get_groups(df: pd.DataFrame, col: str):
@@ -75,3 +76,33 @@ def generate_wordclouds_for_years(filename: str, text_col: str, count_col: str):
 
 def is_word(token: Token):
     return not (token.is_punct or token.is_space)
+
+
+def run_stats_test(dfs: list[pd.DataFrame], col: str):
+    """Automatically decides whether to run a parametric or non-parametric test by 
+    first checking if the given inputs are all normally distributed and have equal variance.
+    Using p-value <= 0.05 for all tests."""
+    
+    samples = [df[col].dropna() for df in dfs]
+
+    def run_non_normal():
+        print("Running test for non-normal distributions")
+        return kruskal(*samples)
+
+    def run_normal():
+        print("Running test for normal distributions")
+        return f_oneway(*samples)
+
+
+    if levene(*samples).pvalue <= 0.05:
+        return run_non_normal()
+    
+    for sample in samples:
+        # Minimum sample size for normaltest
+        if len(sample) < 8:
+            return run_non_normal()
+        
+        if normaltest(sample).pvalue <= 0.05:
+            return run_non_normal()
+    
+    return run_normal()
