c7eb4be68d294dde829d483b6f60a70b05f762bb
Adam Podoxin <adam.podoxin@gmail.com>
2025-12-03 15:29:13 -0800

    Update lexical density to output analysis to one file

--- dataset_config.py
+++ dataset_config.py
@@ -11,7 +11,7 @@ BASE_FAKESPEAK_CONFIG = BASE_CONFIG | {
     "name": "Fakespeak",
     "dir": "./data/Fakespeak-ENG",
     "input_path": "./data/Fakespeak-ENG/Fakespeak-ENG modified.xlsx",
-    "usecols": ["ID", "combinedLabel", "originalTextType", "originalBodyText", "originalDateYear"],
+    "usecols": ["ID", "originalTextType", "originalBodyText", "originalHeadline", "originalDateYear"],
     "id_col": "ID"
 }
 
@@ -19,6 +19,6 @@ BASE_MISINFOTEXT_CONFIG = BASE_CONFIG | {
     "name": "MisInfoText",
     "dir": "./data/MisInfoText",
     "input_path": "./data/MisInfoText/PolitiFact_original_modified.xlsx",
-    "usecols": None,
+    "usecols": ["factcheckURL", "originalTextType", "originalBodyText", "originalHeadline", "originalDateYear"],
     "id_col": "factcheckURL"
 }
--- helpers.py
+++ helpers.py
@@ -6,6 +6,60 @@ from wordcloud import WordCloud
 from scipy.stats import normaltest, levene, f_oneway, kruskal
 
 
+def load_misinfotext():
+    df = pd.read_excel(
+        "./data/MisInfoText/PolitiFact_original_modified.xlsx", 
+        sheet_name="Working",
+        usecols=["factcheckURL", "originalBodyText", "originalHeadline", "originalTextType", "originalDateYear"]
+    )
+
+    df = df.rename(
+        {
+            "factcheckURL": "id",
+            "originalBodyText": "text",
+            "originalHeadline": "headline",
+            "originalTextType": "text_type",
+            "originalDateYear": "year",
+        },
+        axis="columns"
+    )
+    
+    return df
+
+
+def load_fakespeak():
+    df = pd.read_excel(
+        "./data/Fakespeak-ENG/Fakespeak-ENG modified.xlsx",
+        sheet_name="Working",
+        usecols=["ID", "originalBodyText", "originalHeadline", "originalTextType", "originalDateYear"]
+    )
+
+    df = df.rename(
+        {
+            "ID": "id",
+            "originalBodyText": "text",
+            "originalHeadline": "headline",
+            "originalTextType": "text_type",
+            "originalDateYear": "year",
+        },
+        axis="columns"
+    )
+    
+    return df
+
+
+def load_data():
+    df = pd.concat([
+        load_misinfotext(),
+        load_fakespeak()
+    ])
+
+    # Removing 2007 and 2008 years because little data in them
+    df = df[~(df["year"] == 2007) & ~(df["year"] == 2008)]
+
+    return df
+
+
 def get_groups(df: pd.DataFrame, col: str):
     grouped = df.groupby(by=col)
 
@@ -13,38 +67,6 @@ def get_groups(df: pd.DataFrame, col: str):
     dfs = [grouped.get_group(key) for key in keys]
 
     return keys, dfs
-
-
-def make_output_path(dataset_config: dict[str, Any], linguistic_feature_name: str):
-    return (
-        f"{dataset_config['dir']}"
-        f"/{dataset_config['output_dir']}"
-        f"/{dataset_config['name']}"
-        f"_{linguistic_feature_name}.xlsx"
-    )
-
-
-def make_output_path_for_type(dataset_config: dict[str, Any], type: str, linguistic_feature_name: str):
-    type_str = "_".join(str(type).split()).lower()
-
-    output_dir = (
-        f"{dataset_config['dir']}"
-        f"/{dataset_config['output_dir']}"
-        f"/{type_str}"
-    )
-
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
-        print("Created directory", output_dir)
-
-    output_path = (
-        f"{output_dir}"
-        f"/{dataset_config['name']}"
-        f"_{type_str}"
-        f"_{linguistic_feature_name}.xlsx"
-    )
-
-    return output_path
 
 
 def generate_wordclouds_for_years(filename: str, text_col: str, count_col: str):
